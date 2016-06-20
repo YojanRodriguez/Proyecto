b8f60ca266bd9a183dc4493d155cad5d1f49269d
YojanRodriguez <yrodriguezj@ucatolica.ac.cr>
2016-06-20 08:41:42 -0600

    Importes

--- src/nuevoahorcado/NuevoAhorcado.java
+++ src/nuevoahorcado/NuevoAhorcado.java
@@ -7,13 +7,13 @@ package nuevoahorcado;
 
 /**
  *
- * @author Familia
+ * @author Yojan Rodriguez
  */
+import java.util.Scanner;
+import java.util.Random;
 public class NuevoAhorcado {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
